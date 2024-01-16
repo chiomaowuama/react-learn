7b5c91cd56a4891caedb966f0fcefae733390584
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-01-16 15:46:28 +0100

    the second

---
+++ 01basicvites/src/App.jsx
@@ -0,0 +1,13 @@
+import Youtube from "./Youtube"
+function App() {
+
+  return (
+    <>
+    <h1>vite react bone</h1>
+    <h1>vite react bone</h1>
+    <Youtube />
+    </>
+  )
+}
+
+export default App
---
+++ 01basicvites/src/Youtube.jsx
@@ -0,0 +1,6 @@
+function Youtube(){
+    return(
+        <h1>the youtube side</h1>
+    )
+}
+export default Youtube
