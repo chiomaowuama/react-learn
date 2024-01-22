4eb6dba54ad1b470d57f2607fbc1c8d6be7098bb
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-01-22 14:27:01 +0100

    the counter app

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+//  let counter = 15
+ const [counter , setcounter] = useState(15)
+ const addValue = () =>{
+  setcounter(counter + 1)
+  console.log(counter)
+  
+ }
+ const removevalue = () =>{
+  setcounter(counter - 1)
+ }
+  return (
+    <>
+     <h1>react course with chioma {counter} </h1>
+     <h2>counter value: {counter}</h2>
+     <button onClick={addValue} >Add value {counter}</button>{" "}
+     <button onClick={removevalue}>Remove value {counter}</button>
+     <footer>footer:{counter}</footer>
+    </>
+  )
+}
+
+export default App
