72ad8fc0e3d96464a485cbeb3d0d60740274a1b3
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-01-24 11:53:13 +0100

    the bg changer is done

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -12,11 +12,11 @@ function App() {
 
   return (
     <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
-      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-    	  <div className='flex flex-wrap justify-center  shadow-lg bg-white py-2 rounded-3xl'>
-            <button className=' outline-none px-4 py-1 rounded-full text-black shadow-lg' onClick={() => changeColor('red')}>red</button>
-            <button className=' outline-none px-4 py-1 rounded-full text-black shadow-lg' onClick={() => changeColor('green')}>green</button>
-            <button className=' outline-none px-4 py-1 rounded-full text-black shadow-lg' onClick={() => changeColor('blue')}>blue</button>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-4'>
+      <div className='flex flex-wrap gap-2 justify-center  shadow-lg bg-white py-2 px-2 rounded-3xl'>
+            <button className=' outline-none px-4 py-1 rounded-full text-black shadow-lg' onClick={() => changeColor('red')} style={{backgroundColor:'red'}}>red</button>
+            <button className=' outline-none px-4 py-1 rounded-full text-black shadow-lg' onClick={() => changeColor('green')} style={{backgroundColor:'green'}}>green</button>
+            <button className=' outline-none px-4 py-1 rounded-full text-black shadow-lg' onClick={() => changeColor('blue')} style={{backgroundColor:'blue'}}>blue</button>
         </div>
       </div>
 
