54f26a886984bb663e5d777817ffa557addd5d85
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-01-29 10:32:36 +0100

    passwordgenerator complete

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -8,11 +8,12 @@ function App() {
   const [numbersAllowed, setNumbersAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState('');
+  const passwordRef = useRef(null)
 
  const generatePassword = useCallback (() =>{
   // logic
-  let pass=""
-  let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
    if(numbersAllowed) str += "0123456789"
    if(charAllowed) str += "!@#$%^&*+"
@@ -28,7 +29,9 @@ function App() {
 
   const copyPasswordToClipboard = () =>{
     window.navigator.clipboard.writeText(password)
+    passwordRef.current?.select();
   }
+  
  useEffect(()=>{
   generatePassword()
  }, [length,charAllowed, numbersAllowed])
@@ -36,7 +39,7 @@ function App() {
    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
     <h1 className='text-3xl font-bold mb-2 text-center'>Password Generator</h1>
     <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-      <input type="text" placeholder='password' readOnly value={password} className='outline-none w-full py-1 px-3'  />
+      <input type="text" placeholder='password' readOnly value={password} className='outline-none w-full py-1 px-3' ref={passwordRef} />
       <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0' onClick={copyPasswordToClipboard}>copy</button>
     </div>
     <div className='flex text-sm gap-x-2 gap-2'>
