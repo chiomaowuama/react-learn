5858314d5de9d343ba270d4dc16594ed023cd7f1
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-01-31 13:44:50 +0100

    made a few more pages and linked them

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -6,12 +6,15 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import Layout from './layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
       <Route path='about' element={<About/>} />
+      <Route path='contact' element={<Contact/>} />
     </Route>
   )
 )
