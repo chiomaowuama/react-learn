36cd29e6bc058897acc4c7902db8dcf799452ea9
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-02-01 23:55:41 +0100

    more comit

--- 08miniContext/src/context/UserContextProvider.jsx
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 import React from "react";
 import UserContext from "./UserContext";
 const UserContextProvider = (children) => {
-
+     const [User, setUser] = useState(null)
+     return(
+        <UserContext.Provider>
+            
+        </UserContext.Provider>
+     )
 }
 
 export default UserContextProvider
