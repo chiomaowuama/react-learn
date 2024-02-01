93a638b3fca2ee54f925baa4e725f59b0b65a4b6
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-02-01 23:53:03 +0100

    more changes

--- 08miniContext/src/context/UserContextProvider.jsx
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -1,2 +1,8 @@
 import { useState } from "react";
 import React from "react";
+import UserContext from "./UserContext";
+const UserContextProvider = (children) => {
+
+}
+
+export default UserContextProvider
