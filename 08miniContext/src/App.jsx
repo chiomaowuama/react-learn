81ae76edde5d7a21ad172a1f03202b04451c4b0b
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-02-01 23:59:23 +0100

    more

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -4,17 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Profile from './component/Profile'
 import Login from './component/Login'
+import UserContextProvider from './context/UserContextProvider'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <UserContextProvider>
       <h1>React video for context api</h1>
      <Login />
       <Profile />
-    </>
+    </UserContextProvider>
   )
 }
 
