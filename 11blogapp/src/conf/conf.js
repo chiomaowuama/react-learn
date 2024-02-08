62343576d8fa571fc0b078f43f167902bca9b317
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-02-08 13:13:25 +0100

    added bucket id

--- 11blogapp/src/conf/conf.js
+++ 11blogapp/src/conf/conf.js
@@ -1,7 +1,9 @@
 const Conf = {
     appwriteUrl: String(process.env.REACT_APP_APPWRITE_URL ),
     appwriteProjectId : string(process.env.REACT_APP_APPWRITE_PROJECT_ID),
-    appwriteDataId : string(process.env.REACT_APP_APPWRITE_DATABASE_ID )
+    appwriteDataId : string(process.env.REACT_APP_APPWRITE_DATABASE_ID ),
+    appwriteCollectionId : string(process.env.REACT_APP_APPWRITE_COLLECTION_ID ),
+    appwriteBucketId : string(process.env.REACT_APP_APPWRITE_BUCKET_ID)
 
 }
 export default Conf
