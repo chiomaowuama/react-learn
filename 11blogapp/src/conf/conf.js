b86cd27a73ebc222ea4c2395edf64727ca194fa1
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-02-08 13:10:37 +0100

    still commiting things

---
+++ 11blogapp/src/conf/conf.js
@@ -0,0 +1,7 @@
+const Conf = {
+    appwriteUrl: String(process.env.REACT_APP_APPWRITE_URL ),
+    appwriteProjectId : string(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+    appwriteDataId : string(process.env.REACT_APP_APPWRITE_DATABASE_ID )
+
+}
+export default Conf
