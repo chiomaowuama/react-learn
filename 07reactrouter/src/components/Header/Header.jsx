0b0a3296148801f7f2c029fcc6d50642f0226a7f
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-02-19 09:46:15 +0100

    little changes while doing revision

--- 07reactrouter/src/components/Header/Header.jsx
+++ 07reactrouter/src/components/Header/Header.jsx
@@ -13,20 +13,7 @@ export default function Header() {
                             alt="Logo"
                         />
                     </Link>
-                    <div className="flex items-center lg:order-2">
-                        <Link
-                            to="#"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Log in
-                        </Link>
-                        <Link
-                            to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Get started
-                        </Link>
-                    </div>
+                  
                     <div
                         className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
                         id="mobile-menu-2"
@@ -87,9 +74,21 @@ export default function Header() {
                                     User
                                 </NavLink>
                             </li>
-                            
-                            
                         </ul>
+                    </div>
+                    <div className="flex items-center lg:order-2">
+                        <Link
+                            to="#"
+                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Log in
+                        </Link>
+                        <Link
+                            to="#"
+                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Get started
+                        </Link>
                     </div>
                 </div>
             </nav>
