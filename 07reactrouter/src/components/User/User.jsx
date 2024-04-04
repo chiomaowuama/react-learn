70f002e0703f5af154b912d54c185b174b6c2b98
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-04-04 12:26:32 +0100

    commit chnages

--- 07reactrouter/src/components/User/User.jsx
+++ 07reactrouter/src/components/User/User.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {useParams} from "react-router-dom"
 
 function User() {
-    const {userId} = useParams()
+    const {userId} = useParams() // when u want to do dynamic capturing of the userid u ses this hook
   return (
     <div className='bg-orange-500 text-black tect-3xl text-center py-5'>User: {userId}</div>
   )
